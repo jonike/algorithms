a60824d13cbc9b3bba826cc1349bd58dc86b7ce3
frepr183@student.liu.se <frepr183@student.liu.se>
2016-09-22 18:32:40 +0200

    Cleaned union-find. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- Data Structures/union-find.cpp
+++ Data Structures/union-find.cpp
@@ -1,28 +1,24 @@
-/* Disjoint-set data structure. */
-
+/* Disjoint-set data structure ("Union-find"). */
 #include <vector>
-
 using namespace std;
 
-class union_set
-{
+class union_set{
 private:
     vector<int> parent;
 public:
-    union_set(int size){resize(size);}
+    union_set(int size){reset(size);}
 
     inline void unify(int a, int b){
         a = find(a);
         parent[a] = find(b);
     }
 
-    inline void resize(int n){
+    inline void reset(int n){
         parent.resize(n);
-        for(int i = 0; i < n; i++){parent[i] = i;}
+        for(int i = 0; i < n; ++i){parent[i] = i;}
     }
 
-    inline int find(int x)
-    {
+    inline int find(int x){
         int u = parent[x];
         int v = u;
         while(parent[u] != u){u = parent[u];}
