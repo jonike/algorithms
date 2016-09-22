9280b2245069b03fb769ae641b9a7d9cbf7d5b9d
frepr183@student.liu.se <frepr183@student.liu.se>
2016-09-22 18:08:16 +0200

    Disjoint-set data structure / union-find. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

---
+++ Data Structures/union-find.cpp
@@ -0,0 +1,42 @@
+/* Disjoint-set data structure. */
+
+#include <vector>
+
+using namespace std;
+
+class union_set
+{
+private:
+    vector<int> parent;
+public:
+    union_set(int size){resize(size);}
+
+    inline void unify(int a, int b){
+        a = find(a);
+        parent[a] = find(b);
+    }
+
+    inline void resize(int n){
+        parent.resize(n);
+        for(int i = 0; i < n; i++){parent[i] = i;}
+    }
+
+    inline int find(int x)
+    {
+        int u = parent[x];
+        int v = u;
+        while(parent[u] != u){u = parent[u];}
+        while(v != u){
+            parent[x] = u;
+            x = v;
+            v = parent[x];
+        }
+        return u;
+    }
+
+    inline int find_recursive(int x){
+        int& y = parent[x];
+        if(x != y){y = find_recursive(y);}
+        return y;
+    }
+};
