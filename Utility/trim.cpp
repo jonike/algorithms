cef7bc919216495be2374d9695e06b7c924909df
Fredrik Präntare <frepr183@student.liu.se>
2016-10-02 11:12:06 +0200

    Update trim.cpp

--- Utility/trim.cpp
+++ Utility/trim.cpp
@@ -5,7 +5,7 @@ inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v"){return t
 
 // Remove adjacent duplicates. "pred" dictates wether two adjacent characters should be treated as duplicates of each other.
 bool duplicate_pred(const char lhs, const char rhs) {return (lhs == rhs) && (lhs == ' ');}
-inline std::string& remove_adjacent(string& s){
+inline void remove_adjacent(string& s){
     std::string::iterator new_end = std::unique(s.begin(), s.end(), duplicate_pred);
     s.erase(new_end, s.end());
 }
