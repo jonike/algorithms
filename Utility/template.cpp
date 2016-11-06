c3aec5884585c89508b0782772d9400ef43cd36d
Fredrik Präntare <frepr183@student.liu.se>
2016-11-06 10:14:38 +0100

    Update template.cpp

--- Utility/template.cpp
+++ Utility/template.cpp
@@ -1,10 +1,10 @@
-/* Start of header. */
+/* Competitive programming template. */
 #include <bits/stdc++.h>
 #define REP(k,a) for(auto k=decltype(a)(0); k < (a); ++k)
 using namespace std;
 constexpr double PI = 3.14159265358979323846264338327;
 void solve(); int main(){ios::sync_with_stdio(false);cin.tie(NULL);solve();return 0;}
-/* End of header */
+/* End of header. */
 
 void solve(){
 
