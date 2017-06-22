e6ff75bfc35588c31bb64bde9ea5619414c845cd
Fredrik Präntare <prantare@gmail.com>
2017-06-22 16:48:16 +0200

    Update bit-permutations.cpp

--- Miscellaneous Algorithms/bit-permutations.cpp
+++ Miscellaneous Algorithms/bit-permutations.cpp
@@ -1,4 +1,4 @@
-// Given a mask, this will generate the next permutation with same amount of bits set. For example, 0011 => 0101 => 0110, etc.
+// Given a mask, this will generate the next permutation with same amount of bits set.
 uint64_t BitPermute( uint64_t nBitPermutation )
 {
     uint64_t nAnswer = ( nBitPermutation | ( nBitPermutation - 1 ) ) + 1;
